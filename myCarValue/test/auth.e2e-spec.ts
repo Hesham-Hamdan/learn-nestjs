40329b3ca83a7b14fc4b4e4655aa0cb3509fb9a0
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-01-22 20:56:49 +0200

    Add a simple e2e-test auth service

---
+++ myCarValue/test/auth.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { App } from 'supertest/types';
+
+describe('Authentication System', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = 'asdlkjqd@akl.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'alskdfjl' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
